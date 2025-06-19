71a1712d8d53a30dfce9283c8503f40199dcf9f4
eve <Barfalona@gmail.com>
2025-06-18 23:59:31 -0700

    t

--- main.js
+++
@@ -1,4 +0,0 @@
-let num = Math.floor(Math.random() * 22 + 1);
-let favIco = document.querySelector('#rand-fav');
-
-favIco.href = "img/favicons/" + num + ".png"
--- p/gmod/main.js
+++
@@ -1,4 +0,0 @@
-let num = Math.floor(Math.random() * 3 + 1);
-let favIco = document.querySelector('#rand-fav');
-
-favIco.src = "site_gfx/cnt_hd" + num + ".jpg"
